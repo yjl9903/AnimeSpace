f0591b5488133c1d2a3d3a4cfc3e8a000934d2d3
XLor <yjl9903@vip.qq.com>
2022-06-25 14:53:28 +0800

    feat: calendar styles

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,6 +4,8 @@ const weekday = now.getDay();
 const weekDayLocale = ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
 
 const bangumi = useBangumi();
+
+const filterNoCN = (subject: OverviewSubject) => subject.name_cn !== '';
 </script>
 
 <route>
@@ -29,9 +31,27 @@ const bangumi = useBangumi();
         <span font-bold text-lg>{{ weekDayLocale[7 - offset] }}</span>
       </h3>
       <div flex="~ wrap gap4">
-        <div v-for="bgm in bangumi.calendar[7 - offset]" w="120px">
-          <img :src="bgm.images.large" :alt="'picture for ' + bgm.name_cn" />
-          <span>{{ bgm.name_cn !== '' ? bgm.name_cn : bgm.name }}</span>
+        <div
+          v-for="bgm in bangumi.calendar[7 - offset].filter(filterNoCN)"
+          w="160px"
+        >
+          <div w="160px" h="200px" flex="~" items-center justify-start>
+            <img
+              :src="bgm.images.large"
+              :alt="'Picture for ' + bgm.name_cn"
+              object-contain
+              max-w="160px"
+              max-h="200px"
+              rounded-2
+              cursor="pointer"
+            />
+          </div>
+          <a
+            :href="bgm.url"
+            target="_blank"
+            class="text-base hover:text-$c-brand text-sm"
+            >{{ bgm.name_cn !== '' ? bgm.name_cn : bgm.name }}</a
+          >
         </div>
       </div>
     </div>
