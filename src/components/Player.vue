442ce3c4962de06bef1c505d78b3bc0ce5b03557
XLor <yjl9903@vip.qq.com>
2022-06-26 16:20:31 +0800

    fix: player flash

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -8,6 +8,7 @@ const props = defineProps<{ source: SourceInfo; options?: Options }>();
 const { source, options } = toRefs(props);
 
 const container = ref<HTMLElement>();
+const isReady = ref(false);
 const player = ref<Plyr | null>(null);
 
 const portTarget = ref<HTMLElement>();
@@ -26,40 +27,48 @@ watch(container, (container) => {
   if (container) {
     const video = container.querySelector('video');
     if (video) {
+      const isDesktop = window.outerWidth > 1280;
+      const controls: string[] = [
+        'play-large', // The large play button in the center
+        'play', // Play/pause playback
+        'progress', // The progress bar and scrubber for playback and buffering
+        'current-time', // The current time of playback
+        'duration', // The full duration of the media
+        isDesktop ? 'mute' : undefined, // Toggle mute
+        isDesktop ? 'volume' : undefined, // Volume control
+        'settings', // Settings menu
+        'pip', // Picture-in-picture (currently Safari only)
+        'airplay', // Airplay (currently Safari only)
+        isDesktop ? 'download' : undefined, // Show a download button with a link to either the current source or a custom URL you specify in your options
+        'fullscreen' // Toggle fullscreen
+      ].filter(Boolean) as string[];
+
       const plyrOptions: Options = {
-        controls: [
-          'play-large', // The large play button in the center
-          'play', // Play/pause playback
-          'progress', // The progress bar and scrubber for playback and buffering
-          'current-time', // The current time of playback
-          'duration', // The full duration of the media
-          'mute', // Toggle mute
-          'volume', // Volume control
-          'settings', // Settings menu
-          'pip', // Picture-in-picture (currently Safari only)
-          'airplay', // Airplay (currently Safari only)
-          'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
-          'fullscreen' // Toggle fullscreen
-        ],
+        controls,
         ...options?.value
       };
       player.value = new Plyr(video, plyrOptions);
       player.value.source = source.value;
+      player.value.on('canplay', () => {
+        isReady.value = true;
+      });
 
-      const fullscreen = document.querySelector(
-        '.plyr__controls [data-plyr="fullscreen"]'
-      );
-      if (fullscreen) {
-        const btn = document.createElement('button');
-        btn.setAttribute('class', 'plyr__controls__item plyr__control');
-        btn.setAttribute('type', 'button');
-        btn.addEventListener('click', () => {
-          expandFullscreen();
-        });
-        fullscreen.parentElement!.insertBefore(btn, fullscreen);
-        portTarget.value = btn;
-      } else {
-        console.warn('Register window fullscreen fail');
+      if (isDesktop) {
+        const fullscreen = document.querySelector(
+          '.plyr__controls [data-plyr="fullscreen"]'
+        );
+        if (fullscreen) {
+          const btn = document.createElement('button');
+          btn.setAttribute('class', 'plyr__controls__item plyr__control');
+          btn.setAttribute('type', 'button');
+          btn.addEventListener('click', () => {
+            expandFullscreen();
+          });
+          fullscreen.parentElement!.insertBefore(btn, fullscreen);
+          portTarget.value = btn;
+        } else {
+          console.warn('Register window fullscreen fail');
+        }
       }
     }
   }
@@ -85,7 +94,10 @@ onUnmounted(() => {
 <template>
   <div
     ref="container"
-    :class="[isFull && 'fixed top-0 left-0 w-[100vw] h-[100vh] z-100']"
+    v-show="isReady"
+    :class="[
+      isFull ? 'fixed top-0 left-0 w-[100vw] h-[100vh] z-100' : 'w-full h-full'
+    ]"
   >
     <slot></slot>
   </div>
--- src/pages/anime/[id]/Play/[ep].vue
+++ src/pages/anime/[id]/Play/[ep].vue
@@ -15,24 +15,23 @@ const src = computed(() => {
     <h2 font-bold text-xl mb4 pb4 border="b-1 base">
       <span>{{ subject.name_cn }} - 第 {{ ep }} 话</span>
     </h2>
-    <div flex="~ gap8" lt-md:flex-col>
-      <Player
-        v-if="src"
-        :src="src"
-        :options="{}"
-        :source="{
-          type: 'video',
-          sources: [{ src: src, type: 'video/mp4', size: 1080 }]
-        }"
-        mt="4"
-        w="full"
-        aspect="video"
-        aspect-video
-      >
-        <video controls playsinline crossorigin="anonymous"></video>
-      </Player>
+    <div flex="~ gap4 xl:gap8 lt-lg:col" w="lt-lg:full">
+      <div aspect="video" mt="4" w="full">
+        <Player
+          v-if="src"
+          :src="src"
+          :options="{}"
+          :source="{
+            type: 'video',
+            sources: [{ src: src, type: 'video/mp4', size: 1080 }]
+          }"
+          class="w-[640px] h-[360px]"
+        >
+          <video controls playsinline crossorigin="anonymous"></video>
+        </Player>
+      </div>
       <div flex-auto></div>
-      <div v-if="onair" mt4 md:max-w="30%">
+      <div v-if="onair" mt4 max-w="lg:250px">
         <h3 font-bold text-xl mb4>选集播放</h3>
         <ChooseEpisodes :anime="onair" :active="+ep"></ChooseEpisodes>
       </div>
