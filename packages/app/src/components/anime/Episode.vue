cd89d41c06d466509c89268051f25b41df172f71
XLor <yjl9903@vip.qq.com>
2022-07-23 23:56:35 +0800

    feat: display watched ep

--- packages/app/src/components/anime/Episode.vue
+++ packages/app/src/components/anime/Episode.vue
@@ -4,7 +4,12 @@ const props = defineProps<{
   ep: number;
   active?: number;
 }>();
+
 const { active, bgmId, ep } = toRefs(props);
+
+const history = useHistory();
+
+const isWatched = computed(() => history.findHistory(bgmId.value, ep.value));
 </script>
 
 <template>
@@ -15,6 +20,7 @@ const { active, bgmId, ep } = toRefs(props);
     inline-block
     w-18
     text-center
+    text-gray-800
     text-base
     text-sm
     op70
@@ -22,7 +28,13 @@ const { active, bgmId, ep } = toRefs(props);
     px2
     py1
     border="1 base rounded"
-    :class="[ep === active && 'bg-[#fff0ec] text-[#e50914]']"
+    :class="[
+      ep === active
+        ? 'bg-[#fff0ec] text-[#e50914]'
+        : isWatched
+        ? 'bg-[#0ca] bg-op-30 border-[#0ca] border-op50'
+        : ''
+    ]"
   >
     第 {{ ep }} 话
   </router-link>
