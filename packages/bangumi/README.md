a36ac25d43100c68681e7e43aba965b73ff0ad36
XLor <yjl9903@vip.qq.com>
2022-08-02 00:10:14 +0800

    chore: init bangumi package

--- packages/bangumi/README.md
+++ packages/bangumi/README.md
@@ -4,4 +4,4 @@ Bangumi list used for [Anime Paste](https://github.com/XLorPaste/AnimePaste) CLI
 
 ## License
 
-AGPL-3.0 License © 2021 [XLor](https://github.com/yjl9903)
+MIT License © 2021 [XLor](https://github.com/yjl9903)
--- packages/bangumi/build.config.ts
+++ packages/bangumi/build.config.ts
@@ -1,7 +1,7 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-  entries: ['src/index'],
+  entries: ['src/index', 'src/client', 'src/transform', 'src/utils'],
   declaration: true,
   clean: true,
   rollup: {
