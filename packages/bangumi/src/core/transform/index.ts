fe94423d9180baa9af6725417aa9865946049788
XLor <yjl9903@vip.qq.com>
2022-08-02 02:34:56 +0800

    chore: init bangumi-data transform

--- packages/bangumi/src/core/transform/index.ts
+++ packages/bangumi/src/core/transform/index.ts
@@ -1,1 +1,31 @@
-export function transform() {}
+import type { Item } from 'bangumi-data';
+
+import type { BangumiExtension } from '../types';
+
+export interface TransformOption {
+  range?: {
+    begin?: Date;
+
+    end?: Date;
+  };
+
+  compress?: boolean;
+
+  fields?: Array<keyof BangumiExtension>;
+}
+
+export async function transform(option: TransformOption) {
+  const data = importBangumiData();
+  if (!data) {
+    throw new Error('Fail importing bangumi-data');
+  }
+  return;
+}
+
+async function importBangumiData() {
+  try {
+    return (await import('bangumi-data')).default;
+  } catch {
+    return undefined;
+  }
+}
---
+++ packages/bangumi/src/core/types.ts
@@ -0,0 +1,37 @@
+import type { Language } from 'bangumi-data';
+
+export type BangumiType = 'tv' | 'web' | 'movie' | 'ova';
+
+export interface Bangumi {
+  bgmId: string;
+
+  title: string;
+
+  type: BangumiType;
+}
+
+export interface BangumiExtension {
+  titleCN: string;
+
+  titleTranslate: Record<Language, string[]>;
+
+  lang: Language;
+
+  officialSite: string;
+
+  /**
+   * tv/web: 番组开始时间;
+   * movie: 上映日期;
+   * ova: 首话发售时间.
+   */
+  begin: string;
+
+  /**
+   * tv/web: 番组完结时间;
+   * movie: 无意义;
+   * ova: 则为最终话发售时间（未确定则置空）.
+   */
+  end: string;
+
+  comment?: string;
+}
