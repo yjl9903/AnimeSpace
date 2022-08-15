e39d6d29e3566e9dd8eaafed9f9336c78055eefc
XLor <yjl9903@vip.qq.com>
2022-08-15 17:56:01 +0800

    fix(worker): header order

--- functions/utils/index.ts
+++ functions/utils/index.ts
@@ -1,14 +1,14 @@
 export function makeResponse(body: object, init: ResponseInit = {}) {
   return new Response(JSON.stringify({ status: 'Ok', data: body }), {
-    headers: { 'Content-Type': 'application/json', ...init.headers },
-    ...init
+    ...init,
+    headers: { 'Content-Type': 'application/json', ...init.headers }
   });
 }
 
 export function makeErrorResponse(message: string, init: ResponseInit = {}) {
   return new Response(JSON.stringify({ status: 'Error', message }), {
-    headers: { 'Content-Type': 'application/json', ...init.headers },
-    ...init
+    ...init,
+    headers: { 'Content-Type': 'application/json', ...init.headers }
   });
 }
 
