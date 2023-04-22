a1c1a97951ba2de6e0647c6d15690ec39fd6cc6d
XLor <yjl9903@vip.qq.com>
2023-04-23 01:10:49 +0800

    feat: use simptrad package

--- packages/database/src/utils.ts
+++ packages/database/src/utils.ts
@@ -1,11 +1,4 @@
-import cnchar from 'cnchar';
-import trad from 'cnchar-trad';
-
-cnchar.use(trad);
-
-export const simpleToTrad = cnchar.convert.simpleToTrad;
-
-export const tradToSimple = cnchar.convert.tradToSimple;
+export { simpleToTrad, tradToSimple } from 'simptrad';
 
 export function sleep(timeout = 1000): Promise<void> {
   return new Promise((res) => {
