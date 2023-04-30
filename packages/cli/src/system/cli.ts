fcb764a5b64efd806d5bead0a892a8428ab9dc0a
XLor <yjl9903@vip.qq.com>
2023-04-30 22:59:56 +0800

    feat: init plugin cli

--- packages/cli/src/system/cli.ts
+++ packages/cli/src/system/cli.ts
@@ -5,5 +5,8 @@ import { version, description } from '../../package.json';
 
 export async function makeCliApp(system: AnimeSystem) {
   const app = breadc('anime', { version, description });
+  for (const plugin of system.space.plugins) {
+    await plugin.command?.(system, app);
+  }
   return app;
 }
--- packages/core/src/plugin.ts
+++ packages/core/src/plugin.ts
@@ -2,14 +2,16 @@ import type { Breadc } from 'breadc';
 
 import type { AnimeSpace } from './space/types';
 
+import type { AnimeSystem } from './system';
+
 export interface Plugin {
   name: string;
 
   prepare?: (space: AnimeSpace) => Promise<void>;
 
-  command?: (space: AnimeSpace, cli: Breadc<{}>) => Promise<void>;
+  command?: (space: AnimeSystem, cli: Breadc<{}>) => Promise<void>;
 
-  introspect?: (space: AnimeSpace, anime: string) => Promise<void>;
+  introspect?: (space: AnimeSystem, anime: string) => Promise<void>;
 
-  refresh?: (space: AnimeSpace, anime: string) => Promise<void>;
+  refresh?: (space: AnimeSystem, anime: string) => Promise<void>;
 }
