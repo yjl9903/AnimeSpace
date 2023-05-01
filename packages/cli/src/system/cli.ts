0d54f0fa4d7bddfd5703cb773ebdf5fa2c50045f
XLor <yjl9903@vip.qq.com>
2023-05-02 02:24:13 +0800

    feat: create introspect animes

--- packages/cli/src/system/cli.ts
+++ packages/cli/src/system/cli.ts
@@ -1,12 +1,31 @@
-import { breadc } from 'breadc';
+import { type Breadc, breadc } from 'breadc';
 import { AnimeSystem } from '@animespace/core';
 
 import { version, description } from '../../package.json';
 
 export async function makeCliApp(system: AnimeSystem) {
   const app = breadc('anime', { version, description });
+  registerApp(system, app);
   for (const plugin of system.space.plugins) {
     await plugin.command?.(system, app);
   }
   return app;
+}
+
+function registerApp(system: AnimeSystem, app: Breadc<{}>) {
+  app
+    .command('refresh', 'Refresh the local anime system')
+    .option('--introspect')
+    .action(async (options) => {
+      if (options.introspect) {
+        await system.introspect();
+      }
+      return await system.refresh();
+    });
+
+  app
+    .command('introspect', 'Introspect the local anime system')
+    .action(async () => {
+      return await system.introspect();
+    });
 }
--- packages/core/src/system/introspect.ts
+++ packages/core/src/system/introspect.ts
@@ -1,11 +1,24 @@
+import type { Plan } from '../space';
+
 import type { AnimeSystem } from './types';
+
+import { Anime } from './anime';
 
 export async function introspect(system: AnimeSystem) {
   for (const plugin of system.space.plugins) {
     await plugin.introspect?.prepare?.(system);
   }
 
+  const plans = (await system.space.plans()).filter((p) => p.state === 'onair');
+  const animePlans = flatAnimePlan(plans);
+  const animes = animePlans.map((ap) => new Anime(system.space, ap));
+
   for (const plugin of system.space.plugins) {
     await plugin.introspect?.finish?.(system);
   }
+  return animes;
+}
+
+export function flatAnimePlan(plans: Plan[]) {
+  return plans.flatMap((p) => p.onair);
 }
