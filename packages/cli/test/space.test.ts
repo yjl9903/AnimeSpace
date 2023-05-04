03b835431674b99243cbf895ed21cec2c91304d3
XLor <yjl9903@vip.qq.com>
2023-05-04 21:18:41 +0800

    Revert "fix: node prefix does not support"
    This reverts commit 1456909bd55557c54c0f776a38dbed0dc8002cf4.

--- packages/cli/test/space.test.ts
+++ packages/cli/test/space.test.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { describe, it, expect } from 'vitest';
--- packages/core/test/space.test.ts
+++ packages/core/test/space.test.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { rimraf } from 'rimraf';
