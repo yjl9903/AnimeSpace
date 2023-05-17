922258ddd45774874a3080a9f0203a5f31d766bb
XLor <yjl9903@vip.qq.com>
2023-05-18 02:08:30 +0800

    feat: disable local plugin

--- packages/local/src/index.ts
+++ packages/local/src/index.ts
@@ -9,7 +9,7 @@ import {
 } from '@animespace/core';
 
 import { parse } from 'anitomy';
-import { dim, bold, lightYellow, lightGreen } from '@breadc/color';
+import { dim, bold, lightYellow, lightGreen, lightBlue } from '@breadc/color';
 
 const DOT = dim('•');
 
@@ -17,6 +17,8 @@ export const LOCAL = 'Local';
 
 export interface LocalOptions extends PluginEntry {
   directory?: string;
+
+  introspect?: boolean;
 }
 
 export async function Local(options: LocalOptions): Promise<Plugin> {
@@ -66,6 +68,8 @@ export async function Local(options: LocalOptions): Promise<Plugin> {
         );
       },
       async refresh(system, anime) {
+        if (options.introspect === false) return;
+
         const logger = createLogger(system);
         const relatedFiles: LocalFile[] = [];
         files.splice(
@@ -99,7 +103,7 @@ export async function Local(options: LocalOptions): Promise<Plugin> {
               }
             };
             logger.info(
-              `${lightGreen('Moving downloaded file')} ${bold(
+              `${lightBlue('Moving local file')} ${bold(
                 file.filename
               )} to ${bold(video.filename)}`
             );
