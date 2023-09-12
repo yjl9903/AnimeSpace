fd84b7f0909cf63eb19d26b279e5833e532fc2cd
XLor <yjl9903@vip.qq.com>
2023-09-12 23:42:29 +0800

    chore: remove logs

--- packages/core/src/system/introspect.ts
+++ packages/core/src/system/introspect.ts
@@ -28,9 +28,8 @@ export async function introspect(
     await plugin.introspect?.finish?.(system);
   }
 
-  logger.log('');
-  system.printDelta();
   logger.info(lightGreen(`Introspect Anime Space OK`));
+  logger.log('');
   return animes;
 }
 
